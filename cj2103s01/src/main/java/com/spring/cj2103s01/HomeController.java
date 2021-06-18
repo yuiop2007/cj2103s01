d53509fd416f51c132f453ac79848aa456eb957c
green <green@SC-202103101110>
2021-06-18 17:48:49 +0900

    ㅇ

--- cj2103s01/src/main/java/com/spring/cj2103s01/HomeController.java
+++ cj2103s01/src/main/java/com/spring/cj2103s01/HomeController.java
@@ -1,19 +1,9 @@
 package com.spring.cj2103s01;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
